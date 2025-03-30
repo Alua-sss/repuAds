1812068d4f1a628f81e136671ccf211e0d609b8e
mm <242398@astanait.edu.kz>
2025-03-30 10:39:45 +0500

    Added Task4: Recursive factorial function

--- src/Task4.java
+++ src/Task4.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Task4 {
+    public static void main(String[] args) {
+        System.out.print("enter n!:   ");
+        Scanner sc=new Scanner(System.in);
+        int n = sc.nextInt();
+        int result=factorial(n);
+        System.out.println("factorial is: "+result);
+    }
+    /**
+     * Function that computes the factorial by recursion
+     * @param n-the number entered
+     * @ return - n factorial
+     * Time complexity: O(n)
+     */
+    public static int factorial(int n) {
+        if (n==1) {return 1;}
+        return factorial(n-1)*n;
+    }
 }
